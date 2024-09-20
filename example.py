5fd1c151c3467d8742d9f4b59f41dff8d6ab9582
Rajesh <Rajesh3009@proton.me>
2024-09-20 15:00:24 +0000

    Hellp

--- example.py
+++ example.py
@@ -1,1 +1,6 @@
 I am editing the README file. Adding some more details about the project description.
+def git_opeation():
+
+ print("I am adding example.py file to the remote repository.")
+
+git_opeation()
